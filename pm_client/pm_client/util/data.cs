9e506af0782e78780fc01ce0b75dea2a30c18b81
magiCian <3210687274@qq.com>
2021-03-16 11:00:51 +0800

    doc added

---
+++ pm_client/pm_client/util/data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pm_client.util
+{
+    class Meeting
+    {
+        public string Id { get { return "hi"; }set { } }
+        string character;
+        string title;
+        long timestart;
+        long timeend;
+    }
+    class Vote
+    {
+        string Title;
+        string State;
+        string t;
+    }
+}
---
+++ pm_client/pm_client/view/sidebar_btn.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pm_client.view
+{
+    /// <summary>
+    /// sidebar_btn.xaml 的交互逻辑
+    /// </summary>
+    public partial class sidebar_btn : UserControl
+    {
+        public sidebar_btn()
+        {
+            InitializeComponent();
+        }
+    }
+}
