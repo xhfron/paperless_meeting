0c4b7b104544399a0a53ab5e0ed990d043f5ae12
xhfron <xhfron@outlook.com>
2021-05-11 16:39:39 +0800

    跨域

---
+++ server/src/main/java/com/xhfron/paperless/config/CORSConfig.java
@@ -0,0 +1,16 @@
+package com.xhfron.paperless.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+    }
+}
