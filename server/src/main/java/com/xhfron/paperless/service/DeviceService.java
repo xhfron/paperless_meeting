062f0c4950f38300ed25ef2a81840e3c0e48b652
xhfron <xhfron@outlook.com>
2021-06-04 22:42:21 +0800

    cgi重构deviceService

--- server/src/main/java/com/xhfron/paperless/service/DeviceService.java
+++ server/src/main/java/com/xhfron/paperless/service/DeviceService.java
@@ -24,17 +24,21 @@ import java.util.List;
 
 @Component
 public class DeviceService {
-    final private String routerLoginUrl = "http://192.168.8.1/cgi-bin/api/router/login";
-    final private String routerClientListUrl = "http://192.168.8.1/cgi-bin/api/client/list";
-    final private String routerDeviceBlockUrl = "http://192.168.8.1/cgi-bin/api/client/block";
-    final private String routerPassword = "admin";
-    final private String deviceRegisterMacPre= "iptables -I INPUT -m mac --mac-source ";
-    final String host = "192.168.8.1";
-    final String username = "root";
-    final String passwd = "admin";
-    //注意顺序嗷
-    //iptables -I INPUT -m mac --mac-source 7c:76:35:e7:13:05 -j ACCEPT
-    //iptables -I INPUT -m mac ! --mac-source 7c:76:35:e7:13:05 -j DROP
+//    final private String routerLoginUrl = "http://192.168.8.1/cgi-bin/api/router/login";
+//    final private String routerClientListUrl = "http://192.168.8.1/cgi-bin/api/client/list";
+//    final private String routerDeviceBlockUrl = "http://192.168.8.1/cgi-bin/api/client/block";
+//    final private String routerPassword = "admin";
+//    final private String deviceRegisterMacPre= "iptables -I INPUT -m mac --mac-source ";
+    final String host = "http://192.168.8.1/cgi-bin/";
+//    final String username = "root";
+//    final String passwd = "admin";
+//    //注意顺序嗷
+//    //iptables -I INPUT -m mac --mac-source 7c:76:35:e7:13:05 -j ACCEPT
+//    //iptables -I INPUT -m mac ! --mac-source 7c:76:35:e7:13:05 -j DROP
+
+/*
+    ssh效率过低，使用cgi重写本操作
+ */
     private String token;
     @Autowired
     private DeviceDao deviceDao;
@@ -48,90 +52,30 @@ public class DeviceService {
         return deviceDao.getDevices();
     }
 
-    private String getToken() {
-        if(token!=null){
-            return token;
-        }
-        RestTemplate temp = new RestTemplate();
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("pwd", routerPassword);
-        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(body,null);
 
-        Token res= temp.postForObject(routerLoginUrl, request, Token.class);
-        try {
-            if(res.getCode()==0){
-                token = res.getToken();
-            }else {
-                throw new Exception("token获取失败");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return token;
-    }
 
     public RouterInfo getRouterInfo(){
         RouterInfo routerInfo;
         RestTemplate temp = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", getToken());
-        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(headers);
-        RouterInfo info = temp.postForObject(routerClientListUrl, request, RouterInfo.class);
-        return info;
-    }
-
-    private Connection getConnection(){
-        try{
-            Connection connection = new Connection(host);
-
-            connection.connect();
-            if(!connection.authenticateWithPassword(username, passwd)){
-                throw new Exception("登录失败");
-            }
-            return connection;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return  null;
-    }
-    private void exeCmds(List<String> cmds){
-        try {
-            Connection connection = getConnection();
-            for(String cmd: cmds){
-                Session session = connection.openSession();
-                session.execCommand(cmd);
-                if(getOutput){
-                    InputStream stdout = new StreamGobbler(session.getStdout());
-                    BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
-                    while (true) {
-                        String line = br.readLine();
-                        if (line == null)
-                            break;
-                        System.out.println(line);
-                    }
-                }
-                session.close();
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
+        String info = temp.getForObject(host+"l.py", String.class);
+        System.out.println(info);
+        return null;
     }
 
     public Boolean blockNewDevice(){
         try{
             List<DeviceDO> devices = deviceDao.getDevices();
-            List<String> cmds = new LinkedList<>();
+            StringBuffer stringBuffer = new StringBuffer();
             //测试用管理，上线时mac替换为超管设备
-            cmds.add("iptables -I INPUT -m mac ! --mac-source 7C:76:35:E7:13:05 -j DROP");
+            stringBuffer.append("iptables -I INPUT -m mac ! --mac-source 7C:76:35:E7:13:05 -j DROP&&");
             for(DeviceDO device : devices){
-               cmds.add("iptables -I INPUT -m mac --mac-source "
-                        +device.getMac()+" -j ACCEPT");
+              stringBuffer.append("iptables -I INPUT -m mac --mac-source "
+                        +device.getMac()+" -j ACCEPT&&");
             }
+            String res= new RestTemplate().getForObject(host+"d.py?action=god&&cmd="+stringBuffer.toString(),String.class);
+            System.out.println(res);
 
-            exeCmds(cmds);
             return true;
 
          }catch (Exception e){
@@ -141,23 +85,9 @@ public class DeviceService {
     }
 
    public Boolean allowDeviceConnect() {
-       try{
-           List<DeviceDO> devices = deviceDao.getDevices();
-           List<String> cmds = new LinkedList<>();
-//           for(DeviceDO device : devices){
-//               cmds.add("iptables -I INPUT -m mac --mac-source "
-//                       +device.getMac()+" -j ACCEPT");
-//           }
-//           //测试用管理，上线时mac替换为超管设备
-//           cmds.add("iptables -I INPUT -m mac ! --mac-source 7C:76:35:E7:13:05 -j DROP");
-           cmds.add("iptables -F INPUT");
-           exeCmds(cmds);
-           return true;
-
-       }catch (Exception e){
-           e.printStackTrace();
-       }
-       return false;
+        String res= new RestTemplate().getForObject(host+"d.py?action=allow",String.class);
+        System.out.println(res);
+        return true;
    }
 
 
